1eae5faa3df9cc7cfd1a232d1146f7bc9e2c51d6
diamondman3 <mmeyers32@protonmail.com>
2023-09-12 17:37:05 -0700

    Figured out that I shouldn't run this repeatedly.

--- KubeCall.py
+++ KubeCall.py
@@ -1,4 +1,5 @@
 #Use py -3.10 KubeCall.py to run
+#Don't run this too many times in quick succession, results in the pod running out of memory
 
 import os, sys
 import importlib.util
@@ -8,7 +9,7 @@ import time
 from kubernetes import client, config
 
 #Create deployment
-os.system("kubectl apply -f fastchatdep.yaml")
+subprocess.run(["kubectl", "apply", "-f", "fastchatdep.yaml"])
 
 #Wait for pod to be ready
 podReady = False
@@ -19,23 +20,17 @@ while not podReady:
     strPodCheck = podCheck.decode("utf-8")
     if 'Running' in strPodCheck:
         podReady = True
-
 print(strPodCheck)
 
-#Enter pod
+#Enter pod and start fastchat
 podNameIndex = strPodCheck.index("fastchatdep-")
 #It's always 27 characters, fastchatdep-9 numbers-5 alphanumeric
 podNameLength = 27
 podName = strPodCheck[podNameIndex:podNameIndex+podNameLength]
-print(podName)
 
-os.system("kubectl exec -it " + podName + " -- /bin/bash")
-time.sleep(3) #Bad practice
-
-#Manually start Fastchat
-
-#os.system("python3 -m fastchat.serve.cli --model-path lmsys/vicuna-7b-v1.5")
-#Program is halted while I'm ssh'ed into the server. Above line doesn't work as-is.
+time.sleep(30) #Takes a real long time to download fastchat
+os.system("kubectl exec -it " + podName + " -- python3 -m fastchat.serve.cli --model-path lmsys/vicuna-7b-v1.5")
+#Runs out of memory when executed.
 
 #Execute Fastchat prompt
 prompt = "Among us"
